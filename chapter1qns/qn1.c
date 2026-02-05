e7ac70d213666c7f225678c5548f803d93b80e07
haushalpandey7-git <haushalpandey7@gmail.com>
2026-02-05 21:37:50 +0545

    Add files via upload

---
+++ chapter1qns/qn1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    
+    float si, p, t, r;
+    printf("Enter the principal, time, rate: ");
+    scanf("%f %f %f", &p,&t,&r);
+
+    si= (p*t*r)/100;  //formula to calculate simple interest
+
+    printf("The simple interest is= %f", si);
+    return 0;
+}

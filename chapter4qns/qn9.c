62028b7c3230ed8c16371fc62b611b79eb8a24b2
haushalpandey7-git <haushalpandey7@gmail.com>
2026-02-05 21:46:17 +0545

    Add files via upload

---
+++ chapter4qns/qn9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    float v[5], avg = 0;
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        scanf("%f", &v[i]);
+        avg=avg+v[i];
+    }
+
+    avg= avg/5;
+
+    for (i = 0; i < 5; i++)
+        printf("Deviation = %f\n", v[i] - avg);
+
+    return 0;
+}
